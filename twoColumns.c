025a518d08d0fed0657a51d9c95dbf129ec74f6b
Altobert <alberto.sanmartin@usach.cl>
2023-11-14 16:36:43 -0300

    modificaciones a programa

--- twoColumns.c
+++ twoColumns.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define MAXC 100000   /* if you need a constant, #define one (or more) */
 
@@ -31,16 +32,15 @@ int knapsack(int W, int wt[], int val[], int n)
 
 size_t coefficients (FILE *fp, int *a, int *b)
 {
-    char buf[MAXC];         /* buffer for reading each line */ 
-    size_t ncoeff = 0;      /* number of coefficient pairs read */
+    char buf[MAXC];         /* buffering para leer cada linea */ 
+    size_t ncoeff = 0;      /* cantidad de numeros pares leidos */
 
     while (ncoeff < MAXC && fgets (buf, MAXC, fp))  /* read each line */
-        /* if it contains 2 double values */
-        //if (sscanf (buf, "%lf %lf", &a[ncoeff], &b[ncoeff]) == 2)
+        /* si hay dos columnas de valores que leer */
         if (sscanf (buf, "%d %d", &a[ncoeff], &b[ncoeff]) == 2)
-            ncoeff++;       /* increment counter */
+            ncoeff++;       /* se incrementa contador */
 
-    return ncoeff;          /* return total count of pairs read */
+    return ncoeff;          /* return contador total de valores pares leidos */
 }
 
 int main (int argc, char **argv) {
@@ -50,24 +50,27 @@ int main (int argc, char **argv) {
     int  wt[MAXC] = {};
     int capacidad_maxima=0;
 
-    int a[MAXC] = {0}, b[MAXC] = {0};           /* arrays of MAXC doubles */
-    size_t n = 0;                               /* count of doubles returned */
-    /* use filename provided as 1st argument (stdin by default) */
+    clock_t ciniIt, cfinIt;
+
+    int a[MAXC] = {0}, b[MAXC] = {0};           /* arrays con constante maxima de enteros MAXC*/
+    size_t n = 0;                               /* contador de enteros retornados */
+
+    /* se usa para rescatar el nombre del archivo */
     FILE *fp = argc > 1 ? fopen (argv[1], "r") : stdin;
     
     capacidad_maxima = atoi(argv[2]);
     
-    if (!fp) {  /* validate file open for reading */
-        perror ("file open failed");
+    if (!fp) {  /* se valida la lectura del archivo */
+        perror ("apertura de archivo fallida");
         return 1;
     }
 
-    if (!(n = coefficients (fp, a, b))) {   /* validate coeff pairs read */
-        fputs ("error: no double values read from file.\n", stderr);
+    if (!(n = coefficients (fp, a, b))) {   /* validate pares de valores leidos */
+        fputs ("error: sin valores que leer desde archivo.\n", stderr);
         return 1;
     }
 
-    if (fp != stdin)   /* close file if not stdin */
+    if (fp != stdin)   /* se cierra archivo */
         fclose (fp);
 
     
@@ -78,9 +81,13 @@ int main (int argc, char **argv) {
           /* se muestran columnas leidas desde archivo*/
           printf ("a[%zu] : %d   b[%zu] : %d\n", i, a[i], i, b[i]);
     }  
-    printf("capacidad maxima contenedor: %d",capacidad_maxima);
-    printf("\n resultado = %d \n", knapsack(capacidad_maxima, wt , val, n));
+    printf("Capacidad maxima contenedor: %d \n",capacidad_maxima);
 
+    ciniIt = clock();
+    printf("Resultado = %d \n", knapsack(capacidad_maxima, wt , val, n));
+    cfinIt = clock();
+    printf("tiempo solucion optima mochila-contenedor:  %f \n", (double)(cfinIt-ciniIt) / CLOCKS_PER_SEC);
+    printf("\n-------------------\n\n");
 
     return 0;
 }
